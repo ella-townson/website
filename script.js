3102cf8216d696f6ab271e8b60795016c8f0688b
ella-townson <elstownson@gmail.com>
2025-01-02 15:00:48 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-var swiper = new Swiper('.blog-slider', {
-      spaceBetween: 30,
-      effect: 'fade',
-      loop: true,
-      mousewheel: {
-        invert: false,
-      },
-      // autoHeight: true,
-      pagination: {
-        el: '.blog-slider__pagination',
-        clickable: true,
-      }
-    });
+
